6be98c03309519fa64ed24451d7f5a849e38d38f
wadehuber <wadehuber@gmail.com>
2015-02-09 22:25:03 -0700

    Added sum_up

--- Activity4Recursion/src/Activity4.java
+++ Activity4Recursion/src/Activity4.java
@@ -1,8 +1,15 @@
+import Recursion.*;
 
 public class Activity4 {
 
 	public static void main(String[] args) {
 
+		for (int ii=0;ii<10;ii++) {
+			System.out.println("sum_up " + ii + ": " + Recursion.sum_up(ii));
+		}
+		for (int ii=0;ii<10;ii++) {
+			System.out.println("sum_up_tail " + ii + ": " + Recursion.sum_up_tail(ii));
+		}
 	}
 
 }
--- Activity4Recursion/src/Recursion/Recursion.java
+++ Activity4Recursion/src/Recursion/Recursion.java
@@ -2,4 +2,38 @@ package Recursion;
 
 public class Recursion {
 
+	// Class example
+	public static int sum_up(int n) {
+		if (n <=1 ) {
+			return 1;
+		}
+		else {
+			return n + sum_up(n-1);
+		}
+	}
+	
+	// Class example
+	public static int sum_up_tail(int n) {
+		return sum_up_tail(n, 0);
+	}
+	public static int sum_up_tail(int n, int tmp) {
+		if (n <1 ) {
+			return tmp;
+		}
+		else {
+			return sum_up_tail(n-1, tmp+n);
+		}
+	}
+	
+	// Activity 4 int fact(int n) 
+	
+	// Activity 4 int fib(int n) 
+	
+	// Activity 4 int gcd(int num1, int num2)
+
+
+    // Project 4 int power (int x, int y)
+    
+    // Project 4 int ack(int m, int n) 
+	
 }
