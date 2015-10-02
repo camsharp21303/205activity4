ce19603dea7c993f9e53b80cffe13e55f844fdbd
wadehuber <wadehuber@gmail.com>
2015-10-01 22:14:35 -0700

    Updated Activity 4 test driver.

--- Activity4Recursion/src/Activity4.java
+++ Activity4Recursion/src/Activity4.java
@@ -10,6 +10,26 @@ public class Activity4 {
 		for (int ii=0;ii<10;ii++) {
 			System.out.println("sum_up_tail " + ii + ": " + Recursion.sum_up_tail(ii));
 		}
+
+/* Uncomment out the following code to test your functions		
+		System.out.println("Test Activity 4:");
+		System.out.println("Factorial:");
+		for (int ii = 0; ii<10; ii ++) {
+			System.out.print(Recursion.fact(ii) + "   ");
+		}
+
+		System.out.println("\n\nFibonacci:");
+		for (int ii = 0; ii<10; ii ++) {
+			System.out.print(Recursion.fib(ii) + "   ");
+		}
+
+		// Test Euclid's algorithm 
+		System.out.println("\n\nGCD:");
+		System.out.println ("GCD of 96 and 60 is " + Recursion.gcd(96, 60));
+		System.out.println ("GCD of 30 and 10 is " + Recursion.gcd(15, 10));
+		System.out.println ("GCD of 96 and 120 is " + Recursion.gcd(96, 120));
+		System.out.println();
+*/
 	}
 
 }
