710985f58a9d085130cc76f4b0b359f7fa4da537
Wade Huber <wadehuber@gmail.com>
2017-02-15 21:11:02 -0700

    Synced with solutions.

--- Activity4Recursion/src/Activity4.java
+++ Activity4Recursion/src/Activity4.java
@@ -1,35 +1,33 @@
-import recursion.*;
+import recursion.Recursion;
 
-public class Activity4 {
-
-	public static void main(String[] args) {
-
-		for (int ii=0;ii<10;ii++) {
-			System.out.println("sum_up " + ii + ": " + Recursion.sum_up(ii));
-		}
-		for (int ii=1;ii<10;ii++) {
-			System.out.println("sum_up_tail " + ii + ": " + Recursion.sum_up_tail(ii));
-		}
-
-/* Uncomment out the following code to test your functions		
-		System.out.println("Test Activity 4:");
+//		Uncomment this block of code to test Activity 4
+/*
+		System.out.println("\n\nActivity 4:");
+		// Test factorial
 		System.out.println("Factorial:");
 		for (int ii = 1; ii<10; ii ++) {
 			System.out.print(Recursion.fact(ii) + "   ");
 		}
 
+		// Test fibonacci
 		System.out.println("\n\nFibonacci:");
 		for (int ii = 1; ii<10; ii ++) {
 			System.out.print(Recursion.fib(ii) + "   ");
 		}
 
-		// Test Euclid's algorithm 
+		// Test Euclid's GCD algorithm 
 		System.out.println("\n\nGCD:");
 		System.out.println ("GCD of 96 and 60 is " + Recursion.gcd(96, 60));
 		System.out.println ("GCD of 30 and 10 is " + Recursion.gcd(30, 10));
 		System.out.println ("GCD of 96 and 120 is " + Recursion.gcd(96, 120));
-		System.out.println();
-*/
-	}
 
-}
+		// Uncomment out this block of code to test Project 4
+		System.out.println("\n\nTest Project 4:");
+		System.out.println("Power:");
+		for (int ii = 1; ii<4; ii ++) {
+			for (int jj = 0; jj<6; jj ++)  {
+				System.out.print(ii + "^" + jj + "=" + Recursion.power(ii,jj) + "   ");
+			}
+			System.out.println();
+		}
+*/
--- Activity4Recursion/src/Project4.java
+++ Activity4Recursion/src/Project4.java
@@ -5,39 +5,52 @@ import recursion.*;
 public class Project4 {
 
 	public static void main(String[] args) {
-		System.out.println("Test Activity 4 & Project 4:");
+		System.out.println("Recursion Tests:");
 		
-/*		Uncomment this block of code to test Activity 4
-		System.out.println("Test Activity 4:");
+		System.out.println("\nClass examples:");
+		for (int ii=0;ii<10;ii++) {
+			System.out.println("sum_up " + ii + ": " + Recursion.sum_up(ii));
+		}
+		for (int ii=1;ii<10;ii++) {
+			System.out.println("sum_up_tail " + ii + ": " + Recursion.sum_up_tail(ii));
+		}
+
+//		Uncomment this block of code to test Activity 4
+/*
+		System.out.println("\n\nActivity 4:");
+		// Test factorial
 		System.out.println("Factorial:");
 		for (int ii = 1; ii<10; ii ++) {
 			System.out.print(Recursion.fact(ii) + "   ");
 		}
 
+		// Test fibonacci
 		System.out.println("\n\nFibonacci:");
 		for (int ii = 1; ii<10; ii ++) {
 			System.out.print(Recursion.fib(ii) + "   ");
 		}
 
-		// Test Euclid's algorithm 
+		// Test Euclid's GCD algorithm 
 		System.out.println("\n\nGCD:");
 		System.out.println ("GCD of 96 and 60 is " + Recursion.gcd(96, 60));
 		System.out.println ("GCD of 30 and 10 is " + Recursion.gcd(30, 10));
 		System.out.println ("GCD of 96 and 120 is " + Recursion.gcd(96, 120));
-		System.out.println();
-*/
 
-/*		Uncomment this block of code to test Project 4
-		System.out.println("Test Project 4:");
+		// Uncomment out this block of code to test Project 4
+		System.out.println("\n\nTest Project 4:");
 		System.out.println("Power:");
-		for (int ii = 0; ii<4; ii ++) {
+		for (int ii = 1; ii<4; ii ++) {
 			for (int jj = 0; jj<6; jj ++)  {
 				System.out.print(ii + "^" + jj + "=" + Recursion.power(ii,jj) + "   ");
 			}
 			System.out.println();
 		}
-
+*/
+		
+//		Uncomment this block of code to test Activity 4
+/*
 		// Test balance function 
+		System.out.println("\nBalance:");
 		System.out.println("Balance 10 20 = " + Recursion.balance(10, 20));
 		System.out.println("Balance 20 10 = " + Recursion.balance(20, 10));
 		System.out.println("Balance 10 21 = " + Recursion.balance(10, 21));
@@ -51,7 +64,7 @@ public class Project4 {
 		{
 			for (int jj = 0; jj < 5; jj++)
 			{
-				System.out.print(Recursion.Ackerman(ii,jj) + "\t");
+				System.out.print(Recursion.Ackermann(ii,jj) + "\t");
 			}
 			System.out.println();
 		}
@@ -64,6 +77,7 @@ public class Project4 {
 			System.out.print ("How high should I go?  ");
 			int upTo = scan.nextInt();
 			Recursion.playGuessingGame(upTo);
+				
 		}
 */
 
