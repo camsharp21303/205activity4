e37a84e5c8e9732a3981261decc13727c8c506cf
wadgv56140 <wadgv56140@CHO104-INST.cgc.maricopa.edu>
2019-02-06 21:27:03 -0700

    Updates from class.

--- Activity4Recursion/src/recursion/Recursion.java
+++ Activity4Recursion/src/recursion/Recursion.java
@@ -2,40 +2,63 @@ package recursion;
 
 public class Recursion {
 	
-	// Class example
+	// Sum up to N
 	public static int sum_up(int n) {
-		if (n <=0 ) {
+		// Base case
+		if (n <= 0 ) {
 			return 0;
 		}
-		else {
-			return n + sum_up(n-1);
-		}
+		return n + sum_up(n-1);
 	}
-
+	
+	// Tail-recursion
 	public static int sum_up_tail(int n) {
 		return sum_up_tail(n, 0);
 	}
-	public static int sum_up_tail(int n, int tmp) {
-		if (n <1 ) {
-			return tmp;
+	public static int sum_up_tail(int n, int x) {
+		if (n < 1) {
+			return x;
 		}
 		else {
-			return sum_up_tail(n-1, tmp+n);
+			return sum_up_tail(n-1, x + n);
 		}
 	}
 	
-    public static int fact (int n)
-    {
-        int ret;
-         
-        if (n <= 1)
-            ret = 1;
-        else
-            ret = n * fact(n-1);
-         
-        return ret;
-    }
-     
+	// Print up to n
+	public static void print_up_to(int n) {
+		print_up_to(n, 1);
+		System.out.println();
+	}	
+	public static void print_up_to(int n, int current) {
+		System.out.print(current + " ");
+		if (current < n) {
+			print_up_to(n, current+1);
+		}
+	}
+	
+	// Factorial
+	public static int fact(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		else {
+			return n * fact(n-1);
+		}
+	}
+	
+	// Factorial - tail-recursive
+	public static int fact_tail(int n) {
+		return fact_tail(n, 1);
+	}
+	public static int fact_tail(int n, int f) {
+		if (n <= 1) {
+			return f;
+		}
+		else {
+			return (fact_tail(n-1, n * f));
+		}
+	}
+	
     // Activity 4 int fib(int n) 
     public static int fib (int n)
     {
